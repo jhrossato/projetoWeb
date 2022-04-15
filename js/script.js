0ec5f9ea1850e1bf1b6d00aba60bd3787c2d793a
João Henrique Rossato <johrossato@gmail.com>
2022-04-15 20:57:10 -0300

    Efeito de transicao tela inicial

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var showCard = document.querySelector('#show-card');
+var hiddenArrow = document.getElementById("hidden-arrow");
+var hiddenCard = document.getElementById("hidden-card");
+var indexMessages = document.getElementById("index-messages");
+
+
+showCard.addEventListener('click', function () {
+
+    indexMessages.classList.add("hidden");
+    hiddenCard.classList.add("show");
+    hiddenArrow.classList.add("show");
+
+})
