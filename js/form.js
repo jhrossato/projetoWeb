46efebd9456a6fa6c1df8609b919000254c6c3bc
João Henrique Rossato <johrossato@gmail.com>
2022-04-16 21:32:37 -0300

    Validacao do email e repetir senha no formulario

---
+++ js/form.js
@@ -0,0 +1,27 @@
+const btn = document.querySelector("#send");
+
+btn.addEventListener("click", () => {
+    const name = document.querySelector("#input-name");
+    const email = document.querySelector("#input-email");
+    const password = document.querySelector("#input-password");
+    const password2 = document.querySelector("#input-repeat-password");
+    const passwordError = document.querySelector("#passwordError");
+    const emailError = document.querySelector("#emailError");
+    const regex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+    let validEmail = false;
+    let validPassword = false;
+
+    password.value != password2.value ? passwordError.classList.add("show") : (validPassword = true);
+
+    !regex.test(email.value) ? emailError.classList.add("show") : (validEmail = true);
+
+    if (validPassword && validEmail) {
+        passwordError.classList.remove("show");
+        emailError.classList.remove("show");
+        alert("Nome: " + name.value +
+            "\nE-mail: " + email.value +
+            "\nSenha: " + password.value +
+            "\nRepetir a Senha: " + password2.value
+        );
+    }
+});
